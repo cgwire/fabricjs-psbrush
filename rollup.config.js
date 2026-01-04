69949e457e0af2cf455524977bf72afaed9fa7bc
Frank Rousseau <frank@cg-wire.com>
2026-01-05 00:00:29 +0100

    [qa] Move everything to javascript
    due to build problems

--- rollup.config.js
+++ rollup.config.js
@@ -1,34 +1,29 @@
-import resolve from "@rollup/plugin-node-resolve"
-import typescript from '@rollup/plugin-typescript'
-import commonjs from "@rollup/plugin-commonjs"
-import terser from '@rollup/plugin-terser'
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 
-import packageJson from './package.json' with { type: 'json' }
-
-const comments = function(_node, comment) {
-  const { value, type } = comment
-  return type === 'comment2' && /@preserve|@license/i.test(value)
-}
+import packageJson from "./package.json" with { type: "json" };
 
 export default [
   {
-    input: 'lib/index.ts',
+    input: "lib/index.js",
     output: [
       {
         file: packageJson.module,
-        name: 'psbrush',
-        format: 'esm',
-      }
+        name: "psbrush",
+        format: "esm",
+        sourcemap: true,
+      },
     ],
     external: [
       ...Object.keys(packageJson.dependencies || {}),
-      ...Object.keys(packageJson.peerDependencies || {})
+      ...Object.keys(packageJson.peerDependencies || {}),
     ],
     plugins: [
-      resolve(),
-      typescript({
-        tsconfig: 'tsconfig.json'
-      })
-    ]
-  }
-]
+      resolve({
+        browser: true,
+        preferBuiltins: false,
+      }),
+      commonjs(),
+    ],
+  },
+];
